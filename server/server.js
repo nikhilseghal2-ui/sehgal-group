e48d07ec1133fe71671ad54cfcb1b865196501a3
Nikhil Sehgal <nikhilseghal2@gmail.com>
2026-09-20 22:02:23 +0530

    Fix Render server port

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
   res.send("SEHGAL GROUP Server is running!");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(process.env.MONGO_URI)
